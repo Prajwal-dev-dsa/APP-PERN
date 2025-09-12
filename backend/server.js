36a19b431a6cf50c7a3cd5b15539c4defb2602de
Prajwal <prajwal77dwivedi@gmail.com>
2025-09-12 14:39:59 +0530

    Database Connected

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
 
+import { sql } from "./config/db.js";
+
 const app = express();
 
 dotenv.config();
@@ -14,6 +16,25 @@ app.use(cors());
 app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers.
 app.use(morgan("dev")); // log all the requests to the console
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+//connecting to the database
+const initDB = async () => {
+  try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS products (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      image VARCHAR(255) NOT NULL,
+      price DECIMAL(10, 2) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );`;
+    console.log("Products table ensured");
+  } catch (e) {
+    console.log(`Error in DB: ${e.message}`);
+  }
+};
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
