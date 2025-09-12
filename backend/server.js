839c6ad26cc538604c77948ef319980bc4090dfa
Prajwal <prajwal77dwivedi@gmail.com>
2025-09-12 14:47:00 +0530

    Rate Limiting & Bot Detection Functionality Added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 import productRoutes from "./routes/product.route.js";
 
@@ -17,6 +18,42 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers.
 app.use(morgan("dev")); // log all the requests to the console
+
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    // check for spoofed bots
+    // spoofed bots are bots that pretend to be legitimate bots like Googlebot or Bingbot by faking their user-agent or IP address
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 //routes
 app.use("/api/products", productRoutes);
